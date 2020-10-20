5d6aac63ef22a1c7089fcd9d7ee37d4f844be0b6
biliwala <72666349+biliwala@users.noreply.github.com>
2020-10-20 13:37:56 +0800

    r2s_lean_minimal_fix, origin use self friendlywrt

--- 2_clone_source_fix.sh
+++ 2_clone_source_fix.sh
@@ -4,7 +4,7 @@ mkdir friendlywrt-$2
 cd friendlywrt-$2
 repo init -u https://github.com/biliwala/friendlywrt_manifests -b $1 -m $2.xml --repo-url=https://github.com/friendlyarm/repo --no-clone-bundle --depth=1
 repo sync -c --no-tags --no-clone-bundle -j8
-cd friendlywrt/ && git fetch --unshallow
+cd friendlywrt/ && git remote rename self origin && git fetch --unshallow
 git checkout `git branch -va | grep remotes/m | awk '{print $3}' | awk -F\/ '{print $2}'`
 
 [ -f target/linux/rockchip-rk3328/config-4.14 ] && sed -i '/CONFIG_CGROUPS/a\CONFIG_CGROUP_PERF=y' target/linux/rockchip-rk3328/config-4.14
